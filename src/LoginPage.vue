171aaaec734b9b7aea297afaacb11af95934bf4c
Michal K <mkletowski@gmail.com>
2019-04-27 15:46:15 +0200

    add meeting

--- src/LoginPage.vue
+++ src/LoginPage.vue
@@ -4,11 +4,33 @@
        <p>jestes zalogowany {{ username }}</p>
        <!-- emituje zdarzenie logout przy kliknieciu -->
        <button @click="$emit('logout')">Wyloguj</button>
+       <br/><br/>
+       <p>
+         Lista spotkań
+       </p>
+       <button @click="addNewMeeting()">Dodaj nowe spotkanie</button>
+       <ol>
+        <li v-for="item in meetings" :key="item.id">{{ item }}</li>
+      </ol>
    </div>
 </template>
 
 <script>
 export default {
   props: ['username'],
+  data(){
+    return{
+      meetings: [
+        'MWO',
+        'TO',
+        'INNE'
+      ]
+    };
+  },
+  methods: {
+    addNewMeeting(meeting) {
+      this.meetings.push('meeting');
+    }
+  }
 }
 </script>
