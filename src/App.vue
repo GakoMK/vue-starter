c4c297cb3af9f4b3932b3bb98e1e579ea0ad9bbd
Michal K <mkletowski@gmail.com>
2019-04-27 14:06:01 +0200

    first step vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,26 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+  <h2>test</h2>
+  <p>{{ email }}</p>
+  <input type="email" v-model="email">
+  <h2>{{ email }}</h2>
+  <div v-if="email.length < 10">Ale masz krótki adres!</div>
+  <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
+  <div v-else>Twój adres e-mail jest stanowczo za długi.</div>
+  <button @click="alertMyEmail()">Wyświetl mój e-mail w alercie</button>
+  <br /><br />
+
+
+  <div v-if="isHidden">
+    <p>jestes zalogowany</p>
+    <button @click="logMeOut()">Wyloguj</button>
+  </div>
+  <div v-else>
+    <input type="email" v-model="email">
+    <button @click="zalobujByEmail()">Zaloguj</button>
+  </div>
+
+
   </div>
 </template>
 
@@ -9,20 +28,28 @@
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data(){
+	  return{
+		  email: 'test@test.pl',
+		  password: '',
+      isHidden: false
+	  };
+  },
+  methods: {
+    alertMyEmail() {
+        alert(this.email);
+    },
+    zalobujByEmail(){
+        this.isHidden= true
+    },
+    logMeOut(){
+        this.isHidden= false,
+        this.email = ''
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
